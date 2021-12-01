aface132f03ae2646c9cc3bb5ae99b9ca5c4baab
bobomurod <bobomurod.boboev@yandex.ru>
2021-12-01 15:26:16 +0300

    commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# SportInvesting Token Timelock contract
+
+
+
+How to deploy
+
+
+```shell
+yarn
+
+npx hardhat compile
+
+npx hardhat run --network bsctestnet scripts/deploy.js
+```
---
+++ contracts/TokenLock.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @custom:security-contact blockchain@sportinvesting.com
+
+contract Timelock {
+    uint public immutable end;
+    address payable public owner;
+    uint public constant duration = 365 days;
+    constructor() {
+        owner = msg.sender;
+        end = block.timestamp + duration;
+    }
+
+    function deposit(address token, uint256 amount, uint period) external {
+        require(amount >= 0, 'amount is zero');
+
+        IERC20(token).transferFrom(msg.sender, address(this), amount);
+    }
+
+    receive() external payable {}
+
+    function withdraw(address token, uint amount) external {
+        require(msg.sender == owner, 'Only owner');
+        require(block.timestamp >= end, 'Too early');
+        if (token == address(0)) {
+            owner.transfer(amount);
+        } else {
+            IERC20(token).transfer(owner, amount);
+        }
+    }
+
+    function transferOwnership(address payable _newOwner) external {
+        require(msg.sender == owner, 'Only owner');
+        owner = _newOwner;
+    }
+}
